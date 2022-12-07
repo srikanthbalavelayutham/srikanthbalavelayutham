05380a2e7c657613fb53308fdd73518df42c3900
Srikanth_Attacker <fireboy024@gmail.com>
2022-12-07 17:22:37 +0530

    my 1st upload

---
+++ Bank.java
@@ -0,0 +1,30 @@
+class Bank
+{
+	String name="SBI";
+	private int minBalance=5000;
+	float interest=3.5f;
+	
+	public void setminBalance(int amount)
+	{
+		if(amount>5000)
+		{
+			this.minBalance=amount;
+		}
+	}
+	
+	public int display()
+	{
+		return this.minBalance;
+	}
+	
+	public static void main(String[] args)
+	{
+		Bank branchManager=new Bank();
+		System.out.println(branchManager.name);
+		branchManager.openAccount();
+	}
+	public void openAccount()
+	{
+		System.out.println("Check");
+	}
+}
