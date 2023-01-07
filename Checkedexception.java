754be9ff0bbb24b584a252357824844403a0c225
Srikanth_Attacker <fireboy024@gmail.com>
2023-01-07 23:38:11 +0530

    checked exception

---
+++ Checkedexception.java
@@ -0,0 +1,17 @@
+package exceptionhandling;
+
+public class Checkedexception {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.out.println("Program is started");
+		Thread.sleep(3000);
+		System.out.println("Program is in progress...");
+		Thread.sleep(5000);
+		System.out.println("Program is ended");
+		Thread.sleep(2000);
+		System.out.println("Program is exit");
+	}
+
+}
