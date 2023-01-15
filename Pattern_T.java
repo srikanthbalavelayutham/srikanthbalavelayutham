f4273a159878cb656cef91166c6b92be0fd6a917
Srikanth_Attacker <fireboy024@gmail.com>
2023-01-15 23:45:33 +0530

    Pattern programs

---
+++ Pattern_T.java
@@ -0,0 +1,33 @@
+package basics;
+
+public class Pattern_T {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int row=1;row<=9;row++)
+		{
+			System.out.print("* ");
+			
+		}
+		System.out.println();
+		for(int rowcal=1;rowcal<=9;rowcal++)
+		{
+			for(int col=1;col<=4;col++)
+			{
+				System.out.print("  ");
+				if(col==4)
+				{
+					System.out.println("*");
+				}
+			}
+		
+		}
+		{
+			
+		}
+		
+
+	}
+
+}
