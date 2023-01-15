c311724ecf77367d01ae78bd5541d6e604ffff19
Srikanth_Attacker <fireboy024@gmail.com>
2023-01-15 23:40:15 +0530

    Pattern programs

---
+++ Pattern3.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int row=1;row<=5;row++)
+		{
+			for(int col=1;col<row;col++)
+			{
+				System.out.print(" ");
+			}
+			for(int colcap=5;colcap>=row;colcap--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
