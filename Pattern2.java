d6195dee876faca976cb1a9e710dcb12868e3656
Srikanth_Attacker <fireboy024@gmail.com>
2023-01-15 23:39:55 +0530

    Pattern programs

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int row=1;row<=5;row++)
+		{
+			for(int col=1;col<=row;col++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
