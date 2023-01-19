543a49563f73c5767ebab2259f360e5882d82b35
Srikanth_Attacker <fireboy024@gmail.com>
2023-01-19 11:54:52 +0530

    updated linecount,wordcount,charactercount

--- File_Handling_Demo2.java
+++ File_Handling_Demo2.java
@@ -45,14 +45,41 @@ public class File_Handling_Demo2 {
 		FileReader fr=new FileReader(file);
 		BufferedReader br =new BufferedReader(fr);
 		String line=br.readLine();
+		int lineCount=0;
+		int wordcount=0;
+		int charcount=0;
 		
 		while(line!=null)
 		{
+			line.contains(".");
+		
+			
 			System.out.println(line);
 			line=br.readLine();
+			
+			
+			
+			lineCount++;
+			
 		}
 
+		System.out.println(lineCount+"lines in this file");
 		
+		FileReader fr1=new FileReader(file);
+		int r=fr1.read();
+		while(r!=-1)
+		{
+			if((char)r==' ')
+			{
+				wordcount++;
+			}
+			
+			r=fr1.read();
+			charcount++;
+		}
+		
+		System.out.println(wordcount+" words in this file");
+		System.out.println(charcount+" characters in this file");
 	}
 
 }
